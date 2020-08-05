b62ec326e1b1206dc5ebd29703fa1fc395bac976
Vladyslav Krys <vladyslav.krys@gmail.com>
2020-08-05 20:53:55 +0300

    [RMS-5] - Create page with courses. Ability to edit, delete courses

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from "react";
 import "./App.css";
+import Header from "./Layout/Header";
 import Dashboard from "./components/Dashboard";
 import AddCourse from "./components/Course/AddCourse";
+import UpdateCourse from "./components/Course/UpdateCourse";
+import SingleCourse from "./components/Course/SingleCourse";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Header from "./Layout/Header";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
-        <h1>Hello</h1>
         <Route exact path="/dashboard" component={Dashboard}></Route>
         <Route exact path="/addCourse" component={AddCourse}></Route>
+        <Route exact path="/course/:id" component={SingleCourse}></Route>
+        <Route exact path="/updateCourse/:id" component={UpdateCourse}></Route>
       </div>
     </Router>
   );
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,17 +2,40 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { getCourses } from "../actions/courseActions";
 import PropTypes from "prop-types";
+import CourseItem from "./Course/CourseItem";
 
 class Dashboard extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: false,
+    };
+  }
+
   componentDidMount() {
     this.props.getCourses();
+    this.setState({
+      loading: true,
+    });
   }
 
   render() {
-    const { courses } = this.props.courseReducer;
-    return (
-      <div>Look Over Here, {courses.map((course) => course.text + "/ ")}</div>
-    );
+    if (this.state.loading) {
+      const { courses } = this.props.courseReducer;
+      return (
+        <div>
+          <h2>List of courses</h2>
+          <div className="row m-5">
+            {courses.map((course) => (
+              <CourseItem key={course.id} course={course} />
+            ))}
+          </div>
+        </div>
+      );
+    } else {
+      return <div>Loading...</div>;
+    }
   }
 }
 
